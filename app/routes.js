f20cda20a486817cf740360b6773c6557a5490ae
Mark Barlow <markbarlow@users.noreply.github.com>
2016-11-09 17:42:04 +0000

    more eligibilty pages

--- app/routes.js
+++ app/routes.js
@@ -9,3 +9,28 @@ router.get('/', function (req, res) {
 // add your routes here
 
 module.exports = router
+
+
+
+// Branching
+
+router.get('/your-basic-details', function (req, res) {
+
+  // get the answer from the query string (eg. ?certified=no)
+  var certified = req.query.certified;
+
+  if (certified == "no"){
+
+    // redirect to the relevant page
+    res.redirect("/get-document-certified");
+
+  } else {
+
+    // if certified is any other value (or is missing) render the page requested
+    res.render('/your-basic-details');
+
+  }
+
+});
+
+
