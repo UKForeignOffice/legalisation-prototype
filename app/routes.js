c74b7e7ca47ea66c128d889227dc7556409e8ba4
Digital Transformation Unit <DTU@Digitals-MacBook-Pro.local>
2016-11-10 15:56:43 +0000

    address part 1

--- app/routes.js
+++ app/routes.js
@@ -60,3 +60,43 @@ router.get('/your-basic-details', function (req, res) {
 });
 
 
+
+// UK address or not branch
+
+router.get('/your-main-address-uk', function (req, res) {
+
+  // get the answer from the query string (eg. ?certified=no)
+  var is_uk = req.query.is_uk;
+
+  if (is_uk == "false"){
+
+    // redirect to the relevant page
+    res.redirect("/international-main-address");
+
+  } else {
+
+    // if any other value (or is missing) render the page requested. No slash beforehand is essential to render
+    res.render('your-main-address-uk');
+
+  }
+
+});
+
+
+
+// Populate the number in the selected address, UK
+// Passing data into a page, dynamic version
+
+router.get('/your-main-address-uk-selected', function (req, res) {
+
+// get the answer from the query string (?number=1) and set it as a variable so you can use it
+  var number = req.query.number;
+
+// now send that variable to the page which has variable tags for number_display
+  res.render('your-main-address-uk-selected', { 'number_display' : number });
+
+
+});
+
+
+
