5e02887159d70299618c151a39f71dbc1fec5ee9
Mark Barlow <markbarlow@users.noreply.github.com>
2016-11-10 10:28:00 +0000

    routing for copy or original

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,32 @@ module.exports = router
 
 // Branching
 
+
+// Need or No Need to Certify branch
+
+
+router.get('/check-documents-eligible', function (req, res) {
+
+  // get the answer from the query string (eg. ?docformat=original)
+  var docformat = req.query.docformat;
+
+  if (docformat == "original"){
+
+    // redirect to the relevant page
+    res.redirect("/your-basic-details");
+
+  } else {
+
+    // if docformat is any other value (or is missing) render the page requested. No slash beforehand is essential to render
+    res.render('check-documents-eligible');
+
+  }
+
+});
+
+
+// Certified vs. Not Certified branch
+
 router.get('/your-basic-details', function (req, res) {
 
   // get the answer from the query string (eg. ?certified=no)
